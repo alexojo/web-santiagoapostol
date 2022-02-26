c22ba130513250d25a01df2aa3b9e238f1770bb6
Alexander <171914@unsaac.edu.pe>
2022-02-25 23:14:38 -0500

    Hero

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Hero } from './components/hero/Hero'
 
 export const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world
-    </h1>
+    <>
+      <Hero/>
+    </>
   )
 }

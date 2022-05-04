b4bfcfe0ff43cf5b8cbd0fa9480773ef29058c56
Alexander Junior Monzon Montalvo <75278388+alexojo@users.noreply.github.com>
2022-05-03 21:39:02 -0500

    admin screen

--- src/screens/users/screens/admin/AdminScreen.js
+++ src/screens/users/screens/admin/AdminScreen.js
@@ -1,7 +1,13 @@
 import React from 'react'
 
 export const AdminScreen = () => {
-  return (
-    <div>AdminScreen</div>
-  )
+    return (
+        <div className='pl-60 h-screen flex items-center'>
+            <img
+              className="mx-auto h-3/4 w-auto opacity-5  "
+              src={require('../../../../images/Insignia.png')}
+              alt="logo"
+            />
+        </div>
+    )
 }

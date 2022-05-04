2bfd25251285fd78593b78189a9eeb16ad2119b4
Alexander Monzon Montalvo <171914@unsaac.edu.pe>
2022-05-04 14:45:04 -0500

    sidebar

--- src/screens/users/components/SideBar.js
+++ src/screens/users/components/SideBar.js
@@ -13,130 +13,114 @@ export const SideBar = () => {
     }
 
     return (
-        <div class="w-60 h-full shadow-md bg-white absolute" id="sidenavSecExample">
-            <div class="pt-4 pb-4 px-6">
-                <a href="#!">
-                <div class="flex items-center">
-                    <div class="shrink-0">
-                    <img src={require('../../../images/Insignia.png')} class="w-10" alt="Avatar"/>
-                    </div>
-                    <div class="grow ml-3">
-                        <p class="text-sm font-bold text-sky-700">Santiago Apóstol</p>
-                    </div>
+        <div class="w-60 h-full shadow-md bg-white absolute flex flex-col justify-between" id="sidenavSecExample">
+            
+            <div>
+                <div class="py-2 px-6">
+                    <a href="#!">
+                        <div class="flex items-center justify-center mx-auto">
+                            <div class="">
+                                <img src={require('../../../images/Insignia.png')} class="w-8" alt="Avatar"/>
+                            </div>
+                            <div class="ml-3">
+                                <p class="text-sm font-bold text-sky-700">SANTIAGO APÓSTOL</p>
+                            </div>
+                        </div>
+                    </a>
                 </div>
-                </a>
+
+
+                <hr class="mb-2 w-5/6 mx-auto"/>
+
+                <p className='text-base text-gray-700 font-semibold px-7 py-2'> Menu </p>
+                
+                <ul class="relative px-1">
+                    <li class="relative">
+
+                        <NavLink to="/admin">
+
+                            <div class="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-zinc-400 font-medium text-ellipsis whitespace-nowrap rounded hover:text-sky-600 hover:bg-blue-50 transition duration-300 ease-in-out" href="#!" data-mdb-ripple="true" data-mdb-ripple-color="info">
+                                
+                                <i class="fa-solid fa-user-pen w-3 h-3 mr-3"></i>
+                                <span>Estudiantes</span>
+                            </div>
+
+                        </NavLink>
+                    </li>
+                    <li class="relative" id="sidenavSecEx2">
+                        <a class="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-zinc-400 font-medium text-ellipsis whitespace-nowrap rounded hover:text-sky-600 hover:bg-blue-50 transition duration-300 ease-in-out cursor-pointer" data-mdb-ripple="true" data-mdb-ripple-color="info" data-bs-toggle="collapse" data-bs-target="#collapseSidenavSecEx2" aria-expanded="false" aria-controls="collapseSidenavSecEx2">
+                            <i class="fa-solid fa-chalkboard w-3 h-3 mr-3"></i>
+                            <span>Secciones</span>
+                            <svg aria-hidden="true" focusable="false" data-prefix="fas" class="w-3 h-3 ml-auto" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                                <path fill="currentColor" d="M207.029 381.476L12.686 187.132c-9.373-9.373-9.373-24.569 0-33.941l22.667-22.667c9.357-9.357 24.522-9.375 33.901-.04L224 284.505l154.745-154.021c9.379-9.335 24.544-9.317 33.901.04l22.667 22.667c9.373 9.373 9.373 24.569 0 33.941L240.971 381.476c-9.373 9.372-24.569 9.372-33.942 0z"></path>
+                            </svg>
+                        </a>
+
+                    </li>
+                    <li class="relative">
+
+                        <NavLink to="/admin">
+
+                            <div class="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-zinc-400 font-medium text-ellipsis whitespace-nowrap rounded hover:text-sky-600 hover:bg-blue-50 transition duration-300 ease-in-out" href="#!" data-mdb-ripple="true" data-mdb-ripple-color="info">
+                                <i class="fa-solid fa-user-tie w-3 h-3 mr-3"></i>
+                                <span>Docentes</span>
+                            </div>
+
+                        </NavLink>
+                    </li>
+                    <li class="relative">
+                        <a class="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-zinc-400 font-medium text-ellipsis whitespace-nowrap rounded hover:text-sky-600 hover:bg-blue-50 transition duration-300 ease-in-out" href="#!" data-mdb-ripple="true" data-mdb-ripple-color="info">
+                            <i class="fa-solid fa-calendar w-3 h-3 mr-3"></i>
+                            <span>Año escolar</span>
+                        </a>
+                    </li>
+                    <li class="relative">
+                        <a class="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-zinc-400 font-medium text-ellipsis whitespace-nowrap rounded hover:text-sky-600 hover:bg-blue-50 transition duration-300 ease-in-out" href="#!" data-mdb-ripple="true" data-mdb-ripple-color="info">
+                            <i class="fa-solid fa-book w-3 h-3 mr-3"></i>
+                            <span>Cursos</span>
+                        </a>
+                    </li>
+                </ul>
+
+                <p className='text-base text-gray-700 font-semibold px-7 py-2'> General </p>
+                
+
+                <ul class="relative px-1">
+
+                    <li class="relative">
+                        <a class="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-zinc-400 font-medium text-ellipsis whitespace-nowrap rounded hover:text-sky-600 hover:bg-blue-50 transition duration-300 ease-in-out" href="#!" data-mdb-ripple="true" data-mdb-ripple-color="info">
+                            <i class="fa-solid fa-calendar w-3 h-3 mr-3"></i>
+                            <span>Salir </span>
+                        </a>
+                    </li>
+                    <li class="relative">
+                        <a class="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-zinc-400 font-medium text-ellipsis whitespace-nowrap rounded hover:text-sky-600 hover:bg-blue-50 transition duration-300 ease-in-out" href="#!" data-mdb-ripple="true" data-mdb-ripple-color="info">
+                            <i class="fa-solid fa-book w-3 h-3 mr-3"></i>
+                            <span>Modo Oscuro</span>
+                        </a>
+                    </li>
+
+                </ul>
             </div>
-            <ul class="relative px-1">
-                <li class="relative">
+            
+            <div>
+                <hr class="mb-2 w-5/6 mx-auto"/>     
 
-                    <NavLink to="/admin">
-
-                        <div class="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-sky-600 hover:bg-blue-50 transition duration-300 ease-in-out" href="#!" data-mdb-ripple="true" data-mdb-ripple-color="info">
-                            
-                            <i class="fa-solid fa-user-pen w-3 h-3 mr-3"></i>
-                            <span>Estudiantes</span>
+                <div class="py-2 px-6 text-center bottom-0 w-full">
+                    <a href="#!">
+                    <div class="flex items-center">
+                        <div class="shrink-0 ">
+                            <img src="https://mdbcdn.b-cdn.net/img/new/avatars/8.webp" class="rounded-full w-10 border-[3px] border-white shadow-md" alt="Avatar"/>
                         </div>
-
-                    </NavLink>
-                </li>
-                <li class="relative" id="sidenavSecEx2">
-                    <a class="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-sky-600 hover:bg-blue-50 transition duration-300 ease-in-out cursor-pointer" data-mdb-ripple="true" data-mdb-ripple-color="info" data-bs-toggle="collapse" data-bs-target="#collapseSidenavSecEx2" aria-expanded="false" aria-controls="collapseSidenavSecEx2">
-                        <i class="fa-solid fa-chalkboard w-3 h-3 mr-3"></i>
-                        <span>Secciones</span>
-                        <svg aria-hidden="true" focusable="false" data-prefix="fas" class="w-3 h-3 ml-auto" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-                            <path fill="currentColor" d="M207.029 381.476L12.686 187.132c-9.373-9.373-9.373-24.569 0-33.941l22.667-22.667c9.357-9.357 24.522-9.375 33.901-.04L224 284.505l154.745-154.021c9.379-9.335 24.544-9.317 33.901.04l22.667 22.667c9.373 9.373 9.373 24.569 0 33.941L240.971 381.476c-9.373 9.372-24.569 9.372-33.942 0z"></path>
-                        </svg>
-                    </a>
-                    <ul class="relative accordion-collapse collapse" id="collapseSidenavSecEx2" aria-labelledby="sidenavSecEx2" data-bs-parent="#sidenavSecExample">
-                        <li class="relative">
-                            <a href="#!" class="flex items-center text-xs py-4 pl-12 pr-6 h-6 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-sky-600 hover:bg-blue-50 transition duration-300 ease-in-out" data-mdb-ripple="true" data-mdb-ripple-color="info">1° Primaria</a>
-                        </li>
-                        <li class="relative">
-                            <a href="#!" class="flex items-center text-xs py-4 pl-12 pr-6 h-6 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-sky-600 hover:bg-blue-50 transition duration-300 ease-in-out" data-mdb-ripple="true" data-mdb-ripple-color="info">2° Primaria</a>
-                        </li>
-                        <li class="relative">
-                            <a href="#!" class="flex items-center text-xs py-4 pl-12 pr-6 h-6 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-sky-600 hover:bg-blue-50 transition duration-300 ease-in-out" data-mdb-ripple="true" data-mdb-ripple-color="info">3° Primaria</a>
-                        </li>
-                        <li class="relative">
-                            <a href="#!" class="flex items-center text-xs py-4 pl-12 pr-6 h-6 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-sky-600 hover:bg-blue-50 transition duration-300 ease-in-out" data-mdb-ripple="true" data-mdb-ripple-color="info">4° Primaria</a>
-                        </li>
-                        <li class="relative">
-                            <a href="#!" class="flex items-center text-xs py-4 pl-12 pr-6 h-6 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-sky-600 hover:bg-blue-50 transition duration-300 ease-in-out" data-mdb-ripple="true" data-mdb-ripple-color="info">5° Primaria</a>
-                        </li>
-                        <li class="relative">
-                            <a href="#!" class="flex items-center text-xs py-4 pl-12 pr-6 h-6 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-sky-600 hover:bg-blue-50 transition duration-300 ease-in-out" data-mdb-ripple="true" data-mdb-ripple-color="info">6° Primaria</a>
-                        </li>
-                    </ul>
-                </li>
-                <li class="relative">
-
-                    <NavLink to="/admin">
-
-                        <div class="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-sky-600 hover:bg-blue-50 transition duration-300 ease-in-out" href="#!" data-mdb-ripple="true" data-mdb-ripple-color="info">
-                            <i class="fa-solid fa-user-tie w-3 h-3 mr-3"></i>
-                            <span>Docentes</span>
+                        <div class="ml-3">
+                            <p class="text-left text-sm font-semibold text-gray-700">Dante R.</p>
+                            <p class="text-xs font-normal text-zinc-400">Administrador</p>
                         </div>
-
-                    </NavLink>
-                </li>
-            </ul>
-            <hr class="my-2"/>
-
-            <ul class="relative p       x-1">
-                <li class="relative">
-                    <a class="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-sky-600 hover:bg-blue-50 transition duration-300 ease-in-out" href="#!" data-mdb-ripple="true" data-mdb-ripple-color="info">
-                        <svg aria-hidden="true" focusable="false" data-prefix="fas" class="w-3 h-3 mr-3" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-                        <path fill="currentColor" d="M488.6 250.2L392 214V105.5c0-15-9.3-28.4-23.4-33.7l-100-37.5c-8.1-3.1-17.1-3.1-25.3 0l-100 37.5c-14.1 5.3-23.4 18.7-23.4 33.7V214l-96.6 36.2C9.3 255.5 0 268.9 0 283.9V394c0 13.6 7.7 26.1 19.9 32.2l100 50c10.1 5.1 22.1 5.1 32.2 0l103.9-52 103.9 52c10.1 5.1 22.1 5.1 32.2 0l100-50c12.2-6.1 19.9-18.6 19.9-32.2V283.9c0-15-9.3-28.4-23.4-33.7zM358 214.8l-85 31.9v-68.2l85-37v73.3zM154 104.1l102-38.2 102 38.2v.6l-102 41.4-102-41.4v-.6zm84 291.1l-85 42.5v-79.1l85-38.8v75.4zm0-112l-102 41.4-102-41.4v-.6l102-38.2 102 38.2v.6zm240 112l-85 42.5v-79.1l85-38.8v75.4zm0-112l-102 41.4-102-41.4v-.6l102-38.2 102 38.2v.6z"></path>
-                        </svg>
-                        <span>Non-collapsible link</span>
+                    </div>
                     </a>
-                </li>
-                <li class="relative" id="sidenavXxEx2">
-                    <a class="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-sky-600 hover:bg-blue-50 transition duration-300 ease-in-out cursor-pointer" data-mdb-ripple="true" data-mdb-ripple-color="info" data-bs-toggle="collapse" data-bs-target="#collapseSidenavXxEx2" aria-expanded="false" aria-controls="collapseSidenavXxEx2">
-                        <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="comments" class="w-3 h-3 mr-3" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
-                            <path fill="currentColor" d="M416 192c0-88.4-93.1-160-208-160S0 103.6 0 192c0 34.3 14.1 65.9 38 92-13.4 30.2-35.5 54.2-35.8 54.5-2.2 2.3-2.8 5.7-1.5 8.7S4.8 352 8 352c36.6 0 66.9-12.3 88.7-25 32.2 15.7 70.3 25 111.3 25 114.9 0 208-71.6 208-160zm122 220c23.9-26 38-57.7 38-92 0-66.9-53.5-124.2-129.3-148.1.9 6.6 1.3 13.3 1.3 20.1 0 105.9-107.7 192-240 192-10.8 0-21.3-.8-31.7-1.9C207.8 439.6 281.8 480 368 480c41 0 79.1-9.2 111.3-25 21.8 12.7 52.1 25 88.7 25 3.2 0 6.1-1.9 7.3-4.8 1.3-2.9.7-6.3-1.5-8.7-.3-.3-22.4-24.2-35.8-54.5z"></path>
-                        </svg>
-                        <span>Collapsible item 3</span>
-                        <svg aria-hidden="true" focusable="false" data-prefix="fas" class="w-3 h-3 ml-auto" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-                        <path fill="currentColor" d="M207.029 381.476L12.686 187.132c-9.373-9.373-9.373-24.569 0-33.941l22.667-22.667c9.357-9.357 24.522-9.375 33.901-.04L224 284.505l154.745-154.021c9.379-9.335 24.544-9.317 33.901.04l22.667 22.667c9.373 9.373 9.373 24.569 0 33.941L240.971 381.476c-9.373 9.372-24.569 9.372-33.942 0z"></path>
-                        </svg>
-                    </a>
-                <ul class="relative accordion-collapse collapse" id="collapseSidenavXxEx2" aria-labelledby="sidenavXxEx2" data-bs-parent="#sidenavSecExample">
-                    <li class="relative">
-                        <a href="#!" class="flex items-center text-xs py-4 pl-12 pr-6 h-6 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-sky-600 hover:bg-blue-50 transition duration-300 ease-in-out" data-mdb-ripple="true" data-mdb-ripple-color="info">Link 5</a>
-                    </li>
-                    <li class="relative">
-                        <a href="#!" class="flex items-center text-xs py-4 pl-12 pr-6 h-6 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-sky-600 hover:bg-blue-50 transition duration-300 ease-in-out" data-mdb-ripple="true" data-mdb-ripple-color="info">Link 6</a>
-                    </li>
-                </ul>
-                </li>
-                <li class="relative" id="sidenavXxEx3">
-                    <a class="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-sky-600 hover:bg-blue-50 transition duration-300 ease-in-out cursor-pointer" data-mdb-ripple="true" data-mdb-ripple-color="info" data-bs-toggle="collapse" data-bs-target="#collapseSidenavXxEx3" aria-expanded="false" aria-controls="collapseSidenavXxEx3">
-                        <svg aria-hidden="true" focusable="false" data-prefix="fas" class="w-3 h-3 mr-3" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-                            <path fill="currentColor" d="M496 384H64V80c0-8.84-7.16-16-16-16H16C7.16 64 0 71.16 0 80v336c0 17.67 14.33 32 32 32h464c8.84 0 16-7.16 16-16v-32c0-8.84-7.16-16-16-16zM464 96H345.94c-21.38 0-32.09 25.85-16.97 40.97l32.4 32.4L288 242.75l-73.37-73.37c-12.5-12.5-32.76-12.5-45.25 0l-68.69 68.69c-6.25 6.25-6.25 16.38 0 22.63l22.62 22.62c6.25 6.25 16.38 6.25 22.63 0L192 237.25l73.37 73.37c12.5 12.5 32.76 12.5 45.25 0l96-96 32.4 32.4c15.12 15.12 40.97 4.41 40.97-16.97V112c.01-8.84-7.15-16-15.99-16z"></path>
-                        </svg>
-
-                        <span>Collapsible item 4</span>
-                        <svg aria-hidden="true" focusable="false" data-prefix="fas" class="w-3 h-3 ml-auto" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-                        <path fill="currentColor" d="M207.029 381.476L12.686 187.132c-9.373-9.373-9.373-24.569 0-33.941l22.667-22.667c9.357-9.357 24.522-9.375 33.901-.04L224 284.505l154.745-154.021c9.379-9.335 24.544-9.317 33.901.04l22.667 22.667c9.373 9.373 9.373 24.569 0 33.941L240.971 381.476c-9.373 9.372-24.569 9.372-33.942 0z"></path>
-                        </svg>
-                    </a>
-                <ul class="relative accordion-collapse collapse" id="collapseSidenavXxEx3" aria-labelledby="sidenavXxEx3" data-bs-parent="#sidenavSecExample">
-                    <li class="relative">
-                        <a href="#!" class="flex items-center text-xs py-4 pl-12 pr-6 h-6 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-sky-600 hover:bg-blue-50 transition duration-300 ease-in-out" data-mdb-ripple="true" data-mdb-ripple-color="info">Link 7</a>
-                    </li>
-                    <li class="relative">
-                        <a href="#!" class="flex items-center text-xs py-4 pl-12 pr-6 h-6 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-sky-600 hover:bg-blue-50 transition duration-300 ease-in-out" data-mdb-ripple="true" data-mdb-ripple-color="info">Link 8</a>
-                    </li>
-                </ul>
-                </li>
-            </ul>
-            <button 
-                class="text-center bottom-0 absolute w-full"
-                onClick={ handleBack }>
-                <hr class="m-0"/>
-                <p class="py-2 text-sm text-gray-700">Salir</p>
-            </button>
+                </div>
+            </div>
+            
         </div>
     )
 }

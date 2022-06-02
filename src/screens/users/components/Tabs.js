f26093e583ab3b26ca574f3a58aa641b1b664a89
Alexander Monzon Montalvo <171914@unsaac.edu.pe>
2022-06-02 14:28:21 -0500

    tab

--- src/screens/users/components/Tabs.js
+++ src/screens/users/components/Tabs.js
@@ -1,49 +1,69 @@
 import React, { useState } from 'react'
 
-export const Tabs = ({ tabs }) => {
+export const Tabs = ({  }) => {
 
-    const [Menu, setMenu] = useState("second")
+    const [Active, setActive] = useState([true,false,false])
+    
+    const tabs = [
+        {
+            target: "#tabs-home3",
+            text: "Option 1"
+        },
+        {
+            target: "#tabs-profile3",
+            text: "Option 2"
+        },
+        {
+            target: "#tabs-messages3",
+            text: "Option 3"
+        }
+    ];
+
+    const content = [
+
+    ]
+
+    const Activo = ( index ) => {
+        let Aux = new Array(3).fill( false);
+        Aux[ index ] = true;
+        setActive( Aux) 
+    }
 
     return (
         <>
             <div class="mb-4 border-b-2 border-gray-30 dark:border-gray-700 mx-5" >
-                <ul class="flex flex-wrap -mb-px text-sm font-medium text-center nav nav-tabs"  id="tabs-tab3" role="tablist">
-                    
-                    <li class="relative group" role="presentation">
-                        <a class="inline-block p-4 rounded-t-lg border-b-2 border-transparent hover:text-gray-600 dark:hover:text-gray-300 active"
-                        id="tabs-home-tab3" data-bs-toggle="pill" data-bs-target="#tabs-home3" role="tab" aria-controls="tabs-home3" aria-selected="true" href="#tabs-home3"
-                        >Settings xd</a>
+                <ul class="nav nav-tabs flex flex-wrap  text-sm font-medium text-center "  id="tabs-tab3" role="tablist">
+                    {
+                        tabs.map( (tab, index) => (
 
-                        <div className='bg-blue-500 h-[3px] w-full absolute left-0 -bottom-[1px] rounded-t-sm hidden group-hover:flex'></div>
-                    </li>
-                    <li class="relative group" role="presentation">
-                        <a class="inline-block p-4 rounded-t-lg border-b-2 border-transparent hover:text-gray-600 dark:hover:text-gray-300 active"
-                        id="tabs-profile-tab3" data-bs-toggle="pill" data-bs-target="#tabs-profile3" role="tab" aria-controls="tabs-profile3" aria-selected="true" href="#tabs-profile3"
-                        >Settings</a>
+                            <li class="relative group nav-item cursor-pointer" role="presentation" onClick={ () => Activo( index )}>
+                                <a class={`nav-link font-semibold leading-none text-sm inline-block p-3 border-transparent  hover:bg-gray-200 text-gray-400 hover:text-sky-600 uppercase transition duration-300 ease-in-out ${ Active[index] ? 'text-sky-600' : ''}`}
+                                data-bs-toggle="pill" data-bs-target= { tab.target }
+                                > { tab.text }</a>
 
-                        <div className='bg-blue-500 h-[3px] w-full absolute left-0 -bottom-[1px] rounded-t-sm hidden group-hover:flex'></div>
-                    </li>
-                    <li class="relative group" role="presentation">
-                        <a class="inline-block p-4 rounded-t-lg border-b-2 border-transparent hover:text-gray-600 dark:hover:text-gray-300 active"
-                        id="tabs-messages-tab3" data-bs-toggle="pill" data-bs-target="#tabs-messages3" role="tab" aria-controls="tabs-messages3" aria-selected="true" href="#tabs-messages3"
-                        >Settings</a>
-
-                        <div className='bg-blue-500 h-[3px] w-full absolute left-0 -bottom-[1px] rounded-t-sm hidden group-hover:flex'></div>
-                    </li>
+                                { Active[index] && <div className='bg-sky-600 h-[4px] w-full absolute left-0 -bottom-[2px] rounded-t-sm transition duration-300 ease-in-out'></div>}
+                            </li>
+                        ))
+                    }
                 </ul>
             </div>
 
-            <div class="tab-content" id="tabs-tabContent3">
-                <div class="tab-pane fade show active" id="tabs-home3" role="tabpanel" aria-labelledby="tabs-home-tab3">
+
+
+            <div class="tab-content mx-5" >
+                <div class="tab-pane fade show active" id="tabs-home3"  >
                     Tab 1 content button version
                 </div>
-                <div class="tab-pane fade" id="tabs-profile3" role="tabpanel" aria-labelledby="tabs-profile-tab3">
+                <div class="tab-pane fade" id="tabs-profile3" >
                     Tab 2 content button version
                 </div>
-                <div class="tab-pane fade" id="tabs-messages3" role="tabpanel" aria-labelledby="tabs-profile-tab3">
+                <div class="tab-pane fade" id="tabs-messages3" >
                     Tab 3 content button version
                 </div>
             </div>
+
+            
+
 
     
 
